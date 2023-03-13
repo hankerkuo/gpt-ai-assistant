571ef6725f0455376df6bcc7e8cd4707e42e577a
Hank <hankerkuo@gmail.com>
2023-03-14 02:28:08 +0800

    implement double checked locking for prisma client

---
+++ db/prisma-client.js
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client';
+import { logger } from '../utils/index.js';
+
+let prisma = null;
+
+function getPrisma() {
+  if (!prisma) {
+    synchronized(() => {
+      if (!prisma) {
+        logger.info('Creating new PrismaClient');
+        prisma = new PrismaClient();
+      }
+    });
+  }
+  return prisma;
+}
+
+/**
+ * Implements a simple lock to prevent multiple instances of PrismaClient
+ * especially under high concurrency.
+ * @param {function} task 
+ * @returns 
+ */
+async function synchronized(task) {
+  if (!synchronized.lock) {
+    synchronized.lock = true;
+    const result = await task();
+    synchronized.lock = false;
+    return result;
+  } else {
+    // function calls after the lock is released
+    return new Promise(resolve => setImmediate(() => resolve(synchronized(task))));
+  }
+}
+
+export default getPrisma;
