6c7147eb237d2144b6e1c634a8238a825cb85f9c
Hank <hankerkuo@gmail.com>
2023-03-15 00:01:16 +0800

    add complete line auth

--- middleware/auth-line-user.js
+++ middleware/auth-line-user.js
@@ -1,26 +1,55 @@
-import Event from "../app/models/event.js";
-import Context from "../app/context.js";
-import { logger } from "../utils/index.js";
-import { hasTrialPrivilege } from "../db/service/user-service.js";
-import { replyMessage } from "../utils/index.js";
+import Event from '../app/models/event.js';
+import Context from '../app/context.js';
+import { logger } from '../utils/index.js';
+import {
+  isUserExist,
+  getTrialRemainingPrompts,
+  grantTrialPrompts,
+  decreaseTrialPrompts
+} from '../db/service/user-service.js';
+import { replyMessage } from '../utils/index.js';
 
 const authLineUser = async (req, res, next) => {
   const event = new Event(req.body.events[0]);
   const context = new Context(event);
   const userId = context.userId;
-  logger.info("Line user id access: " + userId);
-  if (!await hasTrialPrivilege(userId)) {
-    //TODO: add 10 prompts trial every day
-    logger.info(`Line user: ${userId}, does not have trial privilege`);
+  logger.info(`Line user id access: ${userId}`);
+
+  if (!(await isUserExist(userId))) {
+    logger.info(`Line user: ${userId}, does not exist`);
     replyMessage({
       replyToken: context.replyToken,
       messages: [
-        { type: "text", text: "You do not have trial privilege, please register first!" },
+        {
+          type: 'text',
+          text: 'Greetings! From now on you can use this bot for 10 questions every day!',
+        },
       ],
     });
-    res.status(403).send("Line user does not have trial privilege");
+    await grantTrialPrompts(userId, 10);
+    logger.info(`New user ${userId} Trial privilege granted`);
+    res.status(200).send(`New user ${userId} Trial privilege granted`);
     return;
   }
+
+  if ((await getTrialRemainingPrompts(userId)) === 0) {
+    //TODO: add 10 prompts trial every day
+    logger.info(`Line user: ${userId}, does not have enough trial prompts`);
+    replyMessage({
+      replyToken: context.replyToken,
+      messages: [
+        {
+          type: 'text',
+          text: 'You do not have enough questions to ask, \
+            please register or wait until tomorrow!',
+        },
+      ],
+    });
+    res.status(403).send('Line user does not have enough trial prompts');
+    return;
+  }
+
+  decreaseTrialPrompts(userId, 1);
   next();
 };
 
