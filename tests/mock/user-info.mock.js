b2353f89fbb8f202309100dc122e5718659190a3
Hank <hankerkuo@gmail.com>
2023-03-25 20:42:15 +0800

    add test

---
+++ tests/mock/user-info.mock.js
@@ -0,0 +1,11 @@
+export const mockUserInfo = (userId) => {
+  switch (userId) {
+    case 'trial_id':
+      return {
+        USER_ID: 'trial_id',
+        USER_NAME: 'trial_name',
+      };
+    default:
+      return null;
+  }
+}
