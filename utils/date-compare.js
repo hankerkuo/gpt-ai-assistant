649a8260ec08d954c0cb0e2635e06656c2ea2e2c
Hank <hankerkuo@gmail.com>
2023-04-27 23:28:18 +0800

    change time to use UTC+0 timezone

--- utils/date-compare.js
+++ utils/date-compare.js
@@ -18,5 +18,7 @@ export function createToday8amDate() {
   const year = now.getFullYear();
   const month = now.getMonth();
   const day = now.getDate();
-  return new Date(year, month, day, 8, 0, 0, 0);
+  // our target is to set 8 am UTC+8
+  // map to 0 am UTC+0, assume the image timezone is UTC+0
+  return new Date(year, month, day, 0, 0, 0, 0);
 }
