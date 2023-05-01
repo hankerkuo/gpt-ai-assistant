c283640d6132fb246307c8e03301cce29a417379
Hank <hankerkuo@gmail.com>
2023-05-01 15:45:25 +0800

    re-construct the main event handler and make events be sequentially handled

--- app/app.js
+++ app/app.js
@@ -23,41 +23,48 @@ import Event from './models/event.js';
  * @param {Context} context
  * @returns {Promise<Context>}
  */
-const handleContext = async (context) => (
-  activateHandler(context)
-  || commandHandler(context)
-  || continueHandler(context)
-  || deactivateHandler(context)
-  || deployHandler(context)
-  || docHandler(context)
-  || drawHandler(context)
-  || forgetHandler(context)
-  || enquireHandler(context)
-  || await issueReportHandler(context)
-  || reportHandler(context)
-  || retryHandler(context)
-  || searchHandler(context)
-  || versionHandler(context)
-  || talkHandler(context)
-  || context
-);
+const handleContext = async (context) =>
+  activateHandler(context) ||
+  commandHandler(context) ||
+  continueHandler(context) ||
+  deactivateHandler(context) ||
+  deployHandler(context) ||
+  docHandler(context) ||
+  drawHandler(context) ||
+  forgetHandler(context) ||
+  enquireHandler(context) ||
+  (await issueReportHandler(context)) ||
+  reportHandler(context) ||
+  retryHandler(context) ||
+  searchHandler(context) ||
+  versionHandler(context) ||
+  talkHandler(context) ||
+  context;
 
-const handleEvents = async (events = []) => (
-  (Promise.all(
-    (await Promise.all(
-      (await Promise.all(
-        events
-          .map((event) => new Event(event))
-          .filter((event) => event.isMessage)
-          .filter((event) => event.isText || event.isAudio)
-          .map((event) => new Context(event))
-          .map((context) => context.initialize()),
-      ))
-        .map((context) => (context.error ? context : handleContext(context))),
-    ))
+const handleEvents = async (events = []) => {
+  const initializedContexts = await Promise.all(
+    events
+      .map((event) => new Event(event))
+      .filter((event) => event.isMessage)
+      .filter((event) => event.isText || event.isAudio)
+      .map((event) => new Context(event))
+      .map((context) => context.initialize())
+  );
+
+  const handledContexts = [];
+  for (const context of initializedContexts) {
+    if (context.error) {
+      handledContexts.push(context);
+    } else {
+      handledContexts.push(await handleContext(context));
+    }
+  }
+
+  return Promise.all(
+    handledContexts
       .filter((context) => context.messages.length > 0)
-      .map((context) => replyMessage(context)),
-  ))
-);
+      .map((context) => replyMessage(context))
+  );
+};
 
 export default handleEvents;
