4550b36aca3d03d3f784df95c2d6abb812099b2a
Hank <hankerkuo@gmail.com>
2023-05-01 15:39:41 +0800

    implement function in a more intuitive way

--- app/handlers/issue-report.js
+++ app/handlers/issue-report.js
@@ -12,24 +12,24 @@ import logger from '../../utils/logger.js';
  * @param {Context} context
  * @returns {boolean}
  */
-const inReportMode = async (context) =>
-  (await getChatMode(context.id)) === 'REPORT';
+const inReportMode = async (context) =>{
+  return (await getChatMode(context.id)) === 'REPORT';
+}
+  
 
 /**
  * @param {Context} context
  * @returns {Promise<Context>}
  */
 const exec = async (context) => {
-  let handlerResult =
-    (await inReportMode(context)) &&
-    (async () => {
-      logger.info('user: ', context.id, ' issued report: ', context.event.text);
-      updateHistory(context.id, (history) => history.erase());
-      context.pushText(COMMAND_SYS_REPORT_FINISH.reply, GENERAL_COMMANDS);
-      await handleUserReport(context.id, context.event.text);
-      return context;
-    })();
-
+  let handlerResult = false;
+  if (await inReportMode(context)) {
+    logger.info('user: ', context.id, ' issued report: ', context.event.text);
+    updateHistory(context.id, (history) => history.erase());
+    context.pushText(COMMAND_SYS_REPORT_FINISH.reply, GENERAL_COMMANDS);
+    await handleUserReport(context.id, context.event.text);
+    handlerResult = context;
+  }
   return handlerResult;
 };
 
